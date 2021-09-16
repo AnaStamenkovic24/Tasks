0133dd40a60bc1ae6b8645f481b8b422412c71be
AnaStamenkovic24 <88903096+AnaStamenkovic24@users.noreply.github.com>
2021-09-16 14:06:57 +0200

    Create FoodDao.java

---
+++ SQLnedelja7/FoodDao.java
@@ -0,0 +1,19 @@
+package rs.itbootcamp.dao;
+
+import rs.itbootcamp.model.FoodModel;
+
+import java.util.List;
+
+public interface FoodDao {
+    void insert(FoodModel fm);
+
+    void addToMeal(int mealID, int foodId, double mass);
+
+    void delete(int id);
+
+    void update(FoodModel fm);
+
+    FoodModel getFood(int id);
+
+    List<FoodModel> getAllFood();
+}
