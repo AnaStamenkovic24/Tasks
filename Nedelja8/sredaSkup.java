bca11319969fe2c6c33980fbb639b269eaf3e456
AnaStamenkovic24 <88903096+AnaStamenkovic24@users.noreply.github.com>
2021-09-27 16:34:41 +0200

    Update sredaSkup.java

--- Nedelja8/sredaSkup.java
+++ Nedelja8/sredaSkup.java
@@ -7,3 +7,24 @@
         }
         return div;
     }
+
+  public static boolean anagram(String s1, String s2) {
+        s1 = s1.replaceAll(" ", "");
+        s2 = s2.replaceAll(" ", "");
+        TreeSet<Character> set1 = new TreeSet<>();
+        ArrayList<Character> arr1 = new ArrayList<>();
+        TreeSet<Character> set2 = new TreeSet<>();
+        ArrayList<Character> arr2 = new ArrayList<>();
+
+        for (char c : s1.toCharArray()) {
+            if (!set1.add(c))
+                arr1.add(c);
+        }
+        for (char c : s2.toCharArray()) {
+            if (!set2.add(c))
+                arr2.add(c);
+        }
+        arr1.sort(Comparator.naturalOrder());
+        arr2.sort(Comparator.naturalOrder());
+        return set1.equals(set2) && arr1.equals(arr2);
+    }
